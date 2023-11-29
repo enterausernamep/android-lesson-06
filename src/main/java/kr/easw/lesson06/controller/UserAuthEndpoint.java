d39313bed9bc4c2978e0191b1531e17554373eec
enterausernamep <ejw0823@naver.com>
2023-11-29 15:36:34 +0900

    ì실습ㅎ ë문ì 해ê결

--- src/main/java/kr/easw/lesson06/controller/UserAuthEndpoint.java
+++ src/main/java/kr/easw/lesson06/controller/UserAuthEndpoint.java
@@ -6,16 +6,15 @@ import kr.easw.lesson06.model.dto.UserDataEntity;
 import kr.easw.lesson06.service.UserDataService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
 public class UserAuthEndpoint {
     private final UserDataService userDataService;
+    private final BCryptPasswordEncoder encoder;
 
 
     // JWT 인증을 위해 사용되는 엔드포인트입니다.
@@ -32,10 +31,23 @@ public class UserAuthEndpoint {
 
 
     @PostMapping("/register")
-    public void register(@ModelAttribute UserDataEntity entity) {
-        // 유저 회원가입을 구현하십시오.
-        // 해당 메서드를 작성하기 위해서는, UserDataService와 admin_dashboard.html을 참고하십시오.
-        // 해당 메서드는 register.html에서 사용됩니다.
-        throw new IllegalStateException("Not implemented yet");
+    public ResponseEntity<Object> register(@ModelAttribute UserDataEntity entity) {
+        try {
+            // 사용자가 이미 존재하는지 확인합니다.
+            if (userDataService.isUserExists(entity.getUserId())) {
+                return ResponseEntity.badRequest().body(new ExceptionalResultDto("이미 존재하는 사용자입니다"));
+            }
+            entity.setPassword(encoder.encode(entity.getPassword()));
+
+            // 사용자를 생성합니다.
+            userDataService.createUser(entity);
+            System.out.println("userId, Password"+entity.getUserId()+"," + entity.getPassword());
+
+            // 성공적인 응답 또는 추가 정보를 반환합니다.
+            return ResponseEntity.ok("사용자가 성공적으로 등록되었습니다");
+        } catch (Exception ex) {
+            // 예외를 적절하게 처리합니다. 필요에 따라 이 부분을 사용자 정의할 수 있습니다.
+            return ResponseEntity.badRequest().body(new ExceptionalResultDto(ex.getMessage()));
+        }
     }
 }
